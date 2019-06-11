8a5305b9a9a462040e52e3f721b11c75cfb35eb0
Ricky Lau <rickylaufitness@gmail.com>
2019-06-11 11:50:10 -0400

    created item model & api get route for all items

---
+++ server/api/items.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const {Item} = require('../db/models')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const items = await Item.findAll()
+    res.json(items)
+  } catch (err) {
+    next(err)
+  }
+})
