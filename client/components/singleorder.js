f14b8c3600ab7b7398cb7048197da9dee6b21f74
Ricky Lau <rickylaufitness@gmail.com>
2019-06-17 16:18:59 -0400

    formatted singleorder component render

--- client/components/singleorder.js
+++ client/components/singleorder.js
@@ -9,26 +9,27 @@ class SingleOrder extends Component {
 
     return (
       <div>
-        <div>Date: {order[0]}</div>
+        <div>Date: {order[0].slice(0, 9)}</div>
         <div>
-          {order.map(currItem => {
-            // const currItemName = items.filter(
-            //   item => item.id === currItem.itemId
-            // )[0]
-            const currItemName = items.reduce((accum, item) => {
-              if (item.id === currItem.itemId) {
-                accum = item.name
-              }
-              return accum
-            }, '')
-            // const currItemName = currItemObj.name
-            console.log('CURR ITEMNAME', currItemName)
-            return (
-              <li key={currItem.itemId}>
-                Name:
-                {currItemName}
-              </li>
-            )
+          {order.map((currItem, idx) => {
+            if (idx !== 0) {
+              const currItemName = items.reduce((accum, item) => {
+                if (item.id === currItem.itemId) {
+                  accum = item.name
+                }
+                return accum
+              }, '')
+              // const currItemName = currItemObj.name
+              console.log('CURR ITEMNAME', currItemName)
+              return (
+                <li key={currItem.itemId}>
+                  Name:
+                  {currItemName}, Quantity: {currItem.quantity}, Price: ${' '}
+                  {(currItem.price * currItem.quantity / 100).toFixed(2)},
+                  Single Item Price: $ {(currItem.price / 100).toFixed(2)}
+                </li>
+              )
+            }
           })}
         </div>
         {/* // <li>Name: {items.name}</li> */}
