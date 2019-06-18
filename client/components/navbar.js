689f538c5881a799efa2cb50495b102d8c658ade
Michael Ng <ng.michael.m@gmail.com>
2019-06-18 16:37:12 -0400

    back to working state with css navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,54 +1,90 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import {NavLink as Link} from 'react-router-dom'
 import {logout} from '../store'
+
+// material ui
+
+import HomeIcon from '@material-ui/icons/Home'
+import ShoppingCartIcon from '@material-ui/icons/Shoppingcart'
 
 const Navbar = ({handleClick, isLoggedIn, isAdmin, user}) => (
   <React.Fragment>
     <h1>Vintage Vidya Games!</h1>
+
     <nav>
-      <Link to="/home">Home</Link>
-      <Link to="/cart">Cart</Link>
+      <Link to="/home" className="small-block">
+        <HomeIcon />
+      </Link>
+
+      <Link to="/cart" className="small-block">
+        <ShoppingCartIcon />
+      </Link>
+
       {/* show link to profile after logged in */}
+
       {isLoggedIn && (
         // React.Fragment wraps the child elements but does not add another dom element
+
         <React.Fragment>
-          <Link to="profile">Profile</Link>
-          <Link to="orders">Orders</Link>
+          <Link to="profile" className="block">
+            Profile
+          </Link>
+
+          <Link to="orders" className="block">
+            Orders
+          </Link>
         </React.Fragment>
       )}
+
       {isAdmin && (
         <React.Fragment>
-          <Link to="inventory">Inventory</Link>
+          <Link to="inventory" className="block">
+            Inventory
+          </Link>
         </React.Fragment>
       )}
+
       {isLoggedIn ? (
         <React.Fragment>
           {/* The navbar will show these links after you log in */}
-          <a href="#" onClick={handleClick}>
+
+          <a href="#" onClick={handleClick} className="block">
             Logout
           </a>
         </React.Fragment>
       ) : (
         <React.Fragment>
           {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
+
+          <Link to="/login" className="block">
+            Login
+          </Link>
+
+          <Link to="/signup" className="block">
+            Sign Up
+          </Link>
         </React.Fragment>
       )}
     </nav>
+
     <hr />
   </React.Fragment>
 )
 
 /**
+
  * CONTAINER
+
  */
+
 const mapState = state => {
   return {
     isLoggedIn: !!state.user.id,
+
     isAdmin: !!state.user.admin,
+
     user: state.user
   }
 }
@@ -57,6 +93,7 @@ const mapDispatch = dispatch => {
   return {
     handleClick() {
       dispatch(logout())
+
       localStorage.clear()
     }
   }
@@ -65,11 +102,17 @@ const mapDispatch = dispatch => {
 export default connect(mapState, mapDispatch)(Navbar)
 
 /**
+
  * PROP TYPES
+
  */
+
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
+
   isLoggedIn: PropTypes.bool.isRequired,
+
   isAdmin: PropTypes.bool.isRequired,
+
   user: PropTypes.object.isRequired
 }
