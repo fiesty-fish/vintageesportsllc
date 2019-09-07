a1f89c739c0fedadecf8afbac06125d3a6fd688a
Tal Luigi <luigi.t.agr@gmail.com>
2019-09-07 07:09:20 -0400

    update: centered all items view in itemlist and singleitem components

--- client/components/singleitem.js
+++ client/components/singleitem.js
@@ -70,7 +70,7 @@ class SingleItem extends Component {
   render() {
     const {item} = this.props
     return (
-      <div className="card">
+      <div className="card flex-containee">
         <div className="card-name">
           <h5>{item.name}</h5>
         </div>
