7cf2e9da905a701453437e97466a18225183c670
Ricky Lau <rickylaufitness@gmail.com>
2019-10-27 17:29:54 -0400

    changed var to const in serviceworker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -33,7 +33,7 @@ self.addEventListener('fetch', function(event) {
         // and because we want the browser to consume the response
         // as well as the cache consuming the response, we need
         // to clone it so we have two streams.
-        var responseToCache = response.clone()
+        const responseToCache = response.clone()
 
         caches.open(CACHE_NAME).then(function(cache) {
           cache.put(event.request, responseToCache)
